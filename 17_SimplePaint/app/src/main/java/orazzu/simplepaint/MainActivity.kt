e336169d677dd702691f96b5e23db2bd8e5d15db
0razzu <0razzu@protonmail.com>
2021-10-12 18:23:54 +0600

    17_SimplePaint

---
+++ 17_SimplePaint/app/src/main/java/orazzu/simplepaint/MainActivity.kt
@@ -0,0 +1,12 @@
+package orazzu.simplepaint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(Draw2D(this))
+    }
+}
