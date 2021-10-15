5f3451fdb9f507eb74e2a90ef06eef27ccaa8a6d
0razzu <0razzu@protonmail.com>
2021-10-16 00:11:06 +0600

    24_Camera: fixed rotation

--- 24_Camera/app/src/main/java/orazzu/camera/MainActivity.kt
+++ 24_Camera/app/src/main/java/orazzu/camera/MainActivity.kt
@@ -1,38 +1,46 @@
 package orazzu.camera
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat.startActivityForResult
-
-import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import java.io.File
+import java.time.Instant
 
 
 class MainActivity: AppCompatActivity() {
     private lateinit var takePhotoButton: Button
     private lateinit var photoView: ImageView
     private lateinit var outputFileUri: Uri
-    private val startForResult: ActivityResultLauncher<Intent> =
+    private val takePhotoLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val photoBitmap = result.data?.getParcelableExtra<Bitmap>("data")
                 if (photoBitmap != null)
                     photoView.setImageBitmap(photoBitmap)
-                else
+                else {
                     photoView.setImageURI(outputFileUri)
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
+                        photoView.rotation = 90F
+                }
             } else {
-                Toast.makeText(this@MainActivity, "Wrong result code", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.take_photo_error),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
@@ -47,13 +55,23 @@ class MainActivity: AppCompatActivity() {
         takePhotoButton.setOnClickListener {
             outputFileUri = FileProvider.getUriForFile(
                 this@MainActivity, "orazzu.camera.provider", File(
-                    externalCacheDir,
-                    "test.jpg"
+                    getExternalFilesDir(Environment.DIRECTORY_DCIM),
+                    (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                        Instant.now().toString() else "test") + ".jpg"
                 )
             )
-            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                .putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
-            startForResult.launch(takePhotoIntent)
+
+            try {
+                val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    .putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
+                takePhotoLauncher.launch(takePhotoIntent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.take_photo_error),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
